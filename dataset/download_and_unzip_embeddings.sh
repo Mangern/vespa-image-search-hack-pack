b7c2afea46179f7f57548be1c06065a385c3da8c
Magnus Eide-Fredriksen <magnuseifr@gmail.com>
2025-02-03 13:37:30 +0100

    feat: add dataset scripts

---
+++ dataset/download_and_unzip_embeddings.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Define the URL and the output filename
+URL="https://data.vespa-cloud.com/sample-apps-data/flickr8k_embeddings_vit_b_32.zip"
+OUTPUT_FILE="flickr8k_embeddings_vit_b_32.zip"
+
+# Download the file
+echo "Downloading $URL..."
+curl -O $URL
+
+# Check if the file was downloaded successfully
+if [ -f "$OUTPUT_FILE" ]; then
+  echo "Download completed successfully. Extracting $OUTPUT_FILE..."
+
+  # Unzip the file
+  unzip "$OUTPUT_FILE" && rm "$OUTPUT_FILE"
+
+  # Check if the unzip was successful
+  if [ $? -eq 0 ]; then
+    echo "Extraction completed successfully."
+
+    # Remove the __MACOSX folder if it exists
+    if [ -d "__MACOSX" ]; then
+      echo "Removing __MACOSX folder..."
+      rm -rf __MACOSX
+      echo "__MACOSX folder removed."
+    fi
+
+    # Remove any `._*` files
+    echo "Removing any resource fork files (._*)..."
+    find . -name "._*" -exec rm -f {} \;
+    echo "Resource fork files removed."
+  else
+    echo "Failed to extract the file."
+  fi
+else
+  echo "Failed to download the file."
+fi
